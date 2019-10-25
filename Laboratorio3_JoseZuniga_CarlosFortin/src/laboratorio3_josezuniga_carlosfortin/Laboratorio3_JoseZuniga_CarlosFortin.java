ff9edd5b7a0a8c3251a404fe8ba43a8c0eb67a1d
Carlos Fortin <cafortinl@hotmail.com>
2019-10-25 14:53:01 -0600

    iniciando el menu

--- Laboratorio3_JoseZuniga_CarlosFortin/src/laboratorio3_josezuniga_carlosfortin/Laboratorio3_JoseZuniga_CarlosFortin.java
+++ Laboratorio3_JoseZuniga_CarlosFortin/src/laboratorio3_josezuniga_carlosfortin/Laboratorio3_JoseZuniga_CarlosFortin.java
@@ -1,12 +1,29 @@
 package laboratorio3_josezuniga_carlosfortin;
 
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class Laboratorio3_JoseZuniga_CarlosFortin {
-
+    public static Scanner leer=new Scanner(System.in);
     public static void main(String[] args) {
         
-    }
+        ArrayList<Ejercito> ejercitos=new ArrayList();
+        int opcion=0;
+        while(opcion!=11)
+        {
+            System.out.printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s", "Ingrese una opcion: ","1. Agregar ejercito", "2. Modificar ejercito", "3. Listar ejercitos", "4. Eliminar un ejercito", "5. Agregar soldados", "6. Listar soldados", "7. Eliminar un soldado", "8. Agregar armas", "9. Listar armas", "10. Eliminar un arma", "11. Salir");
+            opcion=leer.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    
+                    break;
+                    
+                case 2:
+                    
+                    break;
+            }
+        }//fin while
+    }//fin main
     
     
     
@@ -50,6 +67,5 @@ public class Laboratorio3_JoseZuniga_CarlosFortin {
     
     
     
-    public ArrayList<Eje
     
 }
